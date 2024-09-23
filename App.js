3568ba049b0929487e97da61b0cb5eef148830b1
devjasin <developer.jasin@gmail.com>
2024-09-23 15:55:32 +0545

    add middleware call multer for file handle

--- App.js
+++ App.js
@@ -2,8 +2,14 @@ const express = require("express");
 const { createBlog } = require("./controller/blogcontroller");
 const app = express();
 const blogRoute = require("./routes/blogRoutes");
+const { blogs } = require("./model/index");
 require("./model/index");
 app.set("view engine", "ejs");
+
+const { multer, storage } = require("./middleware/multerConfig");
+const upload = multer({ storage: storage });
+
+app.use(express.urlencoded({ extended: true }));
 app.listen(3000, () => {
   console.log("server is running at 3000 portnumber");
 });
@@ -27,6 +33,21 @@ app.get("/blog/edit", (req, res) => {
 });
 app.get("/contact", (req, res) => {
   res.render("contact.ejs");
+});
+//main function which insert data into database
+//middleware is always between router and main function
+//incase of array we shoud use upload.array
+app.post("/blog", upload.single("image"), async (req, res) => {
+  const { title, subtitle, description } = req.body;
+  console.log(req.files);
+  console.log(req.res);
+  await blogs.create({
+    title: title,
+    subTitle: subtitle,
+    description: description,
+    imageUrl: req.file.filename,
+  });
+  res.redirect("/");
 });
 
 //incase if we use react for front end than our code will be
